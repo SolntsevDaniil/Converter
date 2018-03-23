20434a51d40d106485d0d6f2f32d763f95bcc6ba
SolntsevDaniil <zapasnoymail1@yandex.ru>
2018-03-23 20:10:45 +0300

    Intermediate version

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* ---------- Start of the program ---------- */
+
+// Declaration of the strict mode
+'use strict';
+
+// Page load ending event
+window.addEventListener('DOMContentLoaded', function() {
+	
+	
+});
