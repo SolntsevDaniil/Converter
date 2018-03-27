eb283b13489487124346b902f9e988fd75401b8f
SolntsevDaniil <zapasnoymail1@yandex.ru>
2018-03-27 19:18:23 +0300

    Some JS added. Input type=number.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,62 @@
 
 // Page load ending event
 window.addEventListener('DOMContentLoaded', function() {
-	
-	
+
+	// Object with methods and properties
+	var temperatures = {
+
+		// Fahrenheit input
+		fahrenheit: document.getElementById('fahrenheit'),
+
+		// Celsius input
+		celsius: document.getElementById('celsius'),
+
+		// Kelvin input
+		kelvin: document.getElementById('kelvin'),
+
+		// From fahrenheit to other measures method
+		fahrenheitCount: function() {
+			this.fahrenheit.addEventListener('input', function() {
+
+				// From String to the Number
+				var num = parseFloat(this.value);
+
+				// Formulas
+				temperatures.celsius.value = (num - 32) / 1.8;
+				temperatures.kelvin.value = ((num - 32) / 1.8) + 273.15;
+			});
+		},
+
+		// From celsius to other measures method
+		celsiusCount: function() {
+			this.celsius.addEventListener('input', function() {
+
+				// From String to the Number
+				var num = parseFloat(this.value);
+
+				// Formulas
+				temperatures.fahrenheit.value = (num * 1.8) + 32;
+				temperatures.kelvin.value = num + 273.15;
+			});
+		},
+
+		// From kelvin to other measures method
+		kelvinCount: function() {
+			this.kelvin.addEventListener('input', function() {
+
+				// From String to the Number
+				var num = parseFloat(this.value);
+
+				// Formulas
+				temperatures.celsius.value = num - 273.15;
+				temperatures.fahrenheit.value = ((num - 273.15) * 1.8) + 32;
+			});
+		}
+	};
+
+
+	// Calling of the methods
+	temperatures.fahrenheitCount();
+	temperatures.celsiusCount();
+	temperatures.kelvinCount();
 });
